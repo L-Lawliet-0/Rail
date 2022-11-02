4b9373025d307bffe0896d7f091e6c9a9ff8e1eb
L-Lawliet-0 <zhang.7199@osu.edu>
2022-11-02 17:28:47 +0800

    city datas excel import

---
+++ Rail/Assets/ExcelData/CityData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CityData
+{
+    public string CityName;
+    public float Population, GDP;
+}
--- Rail/Assets/Scripts/GameLogic/GridData.cs
+++ Rail/Assets/Scripts/GameLogic/GridData.cs
@@ -87,12 +87,14 @@ public class GridData : MonoBehaviour
             GridDatas = bf.Deserialize(file) as List<GridSave>;
         }
 
+        /*
         HashSet<string> gridNames = new HashSet<string>();
         foreach (GridSave gs in GridDatas)
             gridNames.Add(gs.name);
 
         foreach (string str in gridNames)
             Debug.LogError(str);
+        */
     }
 
     /// <summary>
